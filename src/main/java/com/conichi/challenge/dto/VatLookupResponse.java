fae47e9634f1e6abec25934f929d821a593ec078
Fazel Farnia <fazel.farnia@gmail.com>
2019-12-08 18:08:49 +0330

    Add more required methods for testing

--- src/main/java/com/conichi/challenge/dto/VatLookupResponse.java
+++ src/main/java/com/conichi/challenge/dto/VatLookupResponse.java
@@ -25,4 +25,58 @@ public class VatLookupResponse {
     public boolean isValid() {
         return valid;
     }
+
+    public void setCountryCode(String countryCode) {
+        this.countryCode = countryCode;
+    }
+
+    public void setVatNumber(String vatNumber) {
+        this.vatNumber = vatNumber;
+    }
+
+    public void setValid(boolean valid) {
+        this.valid = valid;
+    }
+
+    public void setBusinessName(String businessName) {
+        this.businessName = businessName;
+    }
+
+    public void setBusinessAddress(String businessAddress) {
+        this.businessAddress = businessAddress;
+    }
+
+    @Override
+    public String toString() {
+        return "VatLookupResponse{" +
+                "countryCode='" + countryCode + '\'' +
+                ", vatNumber='" + vatNumber + '\'' +
+                ", valid=" + valid +
+                ", businessName='" + businessName + '\'' +
+                ", businessAddress='" + businessAddress + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof VatLookupResponse)) return false;
+
+        VatLookupResponse that = (VatLookupResponse) o;
+
+        if (isValid() != that.isValid()) return false;
+        if (countryCode != null ? !countryCode.equals(that.countryCode) : that.countryCode != null) return false;
+        if (vatNumber != null ? !vatNumber.equals(that.vatNumber) : that.vatNumber != null) return false;
+        return !(businessName != null ? !businessName.equals(that.businessName) : that.businessName != null);
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = countryCode != null ? countryCode.hashCode() : 0;
+        result = 31 * result + (vatNumber != null ? vatNumber.hashCode() : 0);
+        result = 31 * result + (isValid() ? 1 : 0);
+        result = 31 * result + (businessName != null ? businessName.hashCode() : 0);
+        return result;
+    }
 }
